28ea8e7b98e246a67588a981bf8481ecf6c08efa
NomieC <Fbdarykids@gmail.com>
2023-04-05 15:57:19 +0700

    a bit changes

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+moneyCount.value = 0
 
 function addMoney() {
     var moneyCount = document.getElementById("moneyCount");
 
     moneyCount.value += 1;
     moneyCount.innerHTML = moneyCount.value;
+}
+
+function auto() {
+    i = 0;
+    while(i != 1){
+        addMoney();
+        sleep(1);
+    }
 }
