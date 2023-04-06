9e97f10548058c5c7498bf0dad5d0b1cd27830ef
Relchzen <104548363+Relchzen@users.noreply.github.com>
2023-04-06 23:11:46 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -30,50 +30,71 @@ shopToggle.addEventListener("click", () => {
     }
 })
 
-const moneyCount = document.getElementById("moneyCount");
-const clicker = document.getElementById("clicker");
-const multiplier = document.getElementById("multiplier");
-
-const clickerCount = clicker.getAttribute("clicker");
-const multiplierCount = multiplier.getAttribute("multiplier");
-
 money = 0;
 var moneyadd = 1;
-
+var multiadd = 1;
+let automuch = 0;
 function addMoney() {
     var moneyCount = document.getElementById("moneyCount");
     var money = Number(moneyCount.getAttribute("money"));
+    localStorage.removeItem(money);
     
-    money += moneyadd;
+    money = money + moneyadd * multiadd;
     moneyCount.setAttribute("money", money);
     localStorage.setItem("money", money);
     document.getElementById("moneyCount").innerHTML = money;
 }
 
 function upgradeClicker() {
-    
-}
-
-function upgradeMultiplier() {
-    
-}
-function auto() {
-    i = 0;
-    while(i != 1){
-        addMoney();
-        // sleep(1);
+        var autoclick = document.getElementById('autoclickbtn');
+        var money = Number(moneyCount.getAttribute("moneyCount"));
+        if(automuch >= 1){
+        money = money + 1
+        console.log(money);
+        moneyCount.setAttribute("moneyCount", money);
+        localStorage.setItem("moneyCount", money);
+        document.getElementById("moneyCount").innerHTML = money;
+        setInterval(function() {
+            autoclick.click()
+        }, 1000)
+    }else{
+        window.alert("lol mana autonya")
     }
 }
 
 function upgradepet(){
     var petup = document.getElementById("petbuy");
-    petup = 1000;
+    petup = 500;
     if (money >= petup){
         moneyadd += 1;
-        money -= 1000;
+        money -= 500;
     }else{
         window.alert("mana duitnya")
     }
     document.getElementById("moneyCount").innerHTML = money;
 }
+function upgradeauto(){
+    var autoup = document.getElementById("autobuy");
+    autoup = 500;
+    if (money >= autoup){
+        automuch += 1;
+        money -= 500;
+    }else{
+        window.alert("mana duitnya");
+    }
+    document.getElementById("moneyCount").innerHTML = money;
+    document.getElementById("awokawok").innerHTML = automuch;
+}
 
+function upgrademulti(){
+    var multiup = document.getElementById("multibuy");
+    multiup = 500;
+    if (money >= multiup){
+        multiadd++;
+        money -= 500;
+    }else{
+        window.alert("mana duitnya")
+    }
+    document.getElementById("multi").innerHTML = multiadd;
+    document.getElementById("moneyCount").innerHTML = money;
+}
